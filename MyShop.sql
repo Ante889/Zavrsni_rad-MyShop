bd9685549e2b4b8be61c60dfdc9fff914685f1e2
Ante Filipović <Ante.filipovic72@gmail.com>
2021-06-09 16:47:46 +0200

    Adding row password into users table

--- MyShop.sql
+++ MyShop.sql
@@ -23,6 +23,7 @@ create table users (
 	id int primary key auto_increment not null,
 	name varchar(50) not null,
 	lastname varchar(50) not null,
+	password varchar (50) not null,
 	role varchar(50) not null  DEFAULT 'Users',
 	email varchar(50) 
 	
