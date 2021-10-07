c98701c79b98f25982029d4c7950a1b1a6248384
Ante Filipović <Ante.filipovic72@gmail.com>
2021-10-07 03:16:50 +0200

    Statistics page

--- app/controller/AdminStatisticsController.php
+++ app/controller/AdminStatisticsController.php
@@ -13,8 +13,40 @@ class AdminStatisticsController extends Controller
         }
     }
 
-        public function index()
+    public function index()
     {
-        $this -> view -> render($this->path.'adminTemplate');
+
+        $productsClass= new Products;
+        $allProducts = $productsClass -> selectAll();
+        
+        foreach ($allProducts as $key ) {
+            $productsCount[$key->title]=$productsClass-> innerSelectLimit(
+                ['bought' => 'price',],
+                'products',
+                ['products-bought'],
+                ['bought.product' => $key->id
+                ],999999,0
+            );
+        }
+        $productNames = '';
+        foreach ($productsCount as $key => $values) {
+            if(!empty($values)){
+                $productNames =$productNames . $key . ' ';
+            }
+            foreach($values as $value => $result){
+                $productsPrice[$key][$value] = $result->price;
+            }
+        }
+        $productNames =explode(' ',$productNames);
+        for ($i=0; $i < count($productsPrice) ; $i++) { 
+            $productTotalprice[$i] = [
+                'label' => $productNames[$i],
+                'y' => array_sum($productsPrice[$productNames[$i]])
+            ];
+        }
+
+        $this -> view -> render($this->path.'statistics',[
+            'productTotalprice' => $productTotalprice
+        ]);
     }
 }
