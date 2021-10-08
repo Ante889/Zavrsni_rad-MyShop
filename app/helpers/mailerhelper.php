c1b7c65a827cf8e5fc7418832b1d146c9ee8145a
Ante Filipović <Ante.filipovic72@gmail.com>
2021-10-08 02:58:18 +0200

    Adding phpmailer with composer(Email go to spam folder for now)

---
+++ app/helpers/mailerhelper.php
@@ -0,0 +1,49 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+require_once 'phpmailer/Exception.php';
+require_once 'phpmailer/PHPMailer.php';
+require_once 'phpmailer/SMTP.php';
+
+
+
+class mailerhelper
+{
+    public function sendMail($email, $title, $subject, $msg)
+    {
+        $mail = new PHPMailer(true);
+
+    try {
+        // Server settings
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
+
+        $mail->Username ='ante.online72@gmail.com';  // YOUR gmail email
+        $mail->Password ='';  // YOUR gmail password
+
+        // Sender and recipient settings
+        $mail->setFrom($email, $title);
+        $mail->addAddress($email, $title);
+        $mail->addReplyTo($email, $title); // to set the reply to
+
+        // Setting the email content
+        $mail->IsHTML(true);
+        $mail->Subject = $subject;
+        $mail->Body = $msg;
+        //$mail->AltBody = 'Plain text message body for non-HTML email client. Gmail SMTP email body.';
+
+        $mail->send();
+
+    } catch (Exception $e) {
+        echo "Error";
+    }
+    }
+}
+
+
+
+?>
